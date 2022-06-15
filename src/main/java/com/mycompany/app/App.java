ca5324e7240991d5abd7300ce1151795a4113e4c
divoncloud <103150375+divoncloud@users.noreply.github.com>
2022-06-14 21:21:15 -0400

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!...Testing Changes V2.0" );
+        System.out.println( "Hello World!...Testing Changes V3.0" );
     }
 }
 
